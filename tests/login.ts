fe695e3f79b919ac325ed899c7542d1331f59db0
Michael Longworth <mlongworth87@gmail.com>
2026-04-08 19:07:04 +0100

    First stab

---
+++ tests/login.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login_page';
+
+
+test('clean login', async ({page}) => {
+    lpage : LoginPage;
+    lPage = new LoginPage();
+
+    
+    
+})
